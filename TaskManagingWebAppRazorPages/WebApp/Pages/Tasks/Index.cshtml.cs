b10148a66b027c03f2fce11e1ac712da68c2009e
brajankukk <brkukk@taltech.ee>
2024-04-12 09:52:43 +0300

    Razor pages completed

--- TaskManagingWebAppRazorPages/WebApp/Pages/Tasks/Index.cshtml.cs
+++ TaskManagingWebAppRazorPages/WebApp/Pages/Tasks/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DAL;
 using Domain;
+using Humanizer;
 
 namespace WebApp.Pages.Tasks
 {
@@ -20,11 +21,31 @@ namespace WebApp.Pages.Tasks
         }
 
         public IList<ToDoTasks> ToDoTasks { get;set; } = default!;
+        
 
         public async Task OnGetAsync()
         {
             ToDoTasks = await _context.ToDoTasks.ToListAsync();
             ToDoTasks = ToDoTasks.OrderBy(m => m.HasToBeDoneAtDt).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return RedirectToPage("./Index");
+            }
+            var todoTask = await _context.ToDoTasks.FindAsync(id);
+
+            if (todoTask == null)
+            {
+                return RedirectToPage("./Index");
+            }
+            todoTask.CompletedAtDt = DateTime.Now;
+            _context.ToDoTasks.Update(todoTask);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
